091fdf8fe9770d1d51eb714ce0622d306f1ccd7e
aditya dhanraj tiwari <aditya.d.tiwari@globallogic.com>
2022-12-23 01:31:23 +0530

    fix1

--- AutomationTestingFramework/src/test/java/tests/StandAloneTest.java
+++ AutomationTestingFramework/src/test/java/tests/StandAloneTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class StandAloneTest {
 
@@ -17,6 +19,8 @@ public class StandAloneTest {
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver= new ChromeDriver();
+		WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(5));
+		
 		driver.get("https://rahulshettyacademy.com/client");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
@@ -27,12 +31,18 @@ public class StandAloneTest {
 		
 		//capture the product list...
 		//instead of iterating over list we are using streams here....
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
 		List<WebElement> l= driver.findElements(By.cssSelector(".mb-3"));
 		
 		
 		WebElement ele= l.stream().filter(p->p.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst().orElse(null);
 		ele.findElement(By.cssSelector(".card-body button:last-of-type")).click();
 		
+		//WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(5));
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[role='alertdialog'][aria-label='Product Added To Cart']")));
+		driver.findElement(By.cssSelector("[routerlink='/dashboard/cart']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("")));
 		
 		
 		
